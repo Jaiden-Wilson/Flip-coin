2af00da25c72c3b0092a03b9cb2acc9fd0269a34
Jaiden-Wilson <jwilson.jaw.10@gmail.com>
2025-07-01 01:17:57 -0400

    Updated styles.css and index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+$("button").click(function(){
+    var num=Math.floor(Math.random()*2);
+    console.log(num);
+    $("#coin").fadeOut(500);
+    
+    $("#coin").fadeIn(500);
+    $("#shadow").animate({width:'-20vw'}, 500);
+    $("#shadow").animate({width:'7vw'}, 500);
+
+    if(num===0){
+        
+        $("#coin").attr("src","./resources/heads.svg");
+        $("#result").html("Heads");
+    }
+    else{
+
+        $("#coin").attr("src","./resources/tails.svg");
+        $("#result").html("Tails");
+
+
+    }
+
+});
+$("#coin").click(function(){
+    var num=Math.floor(Math.random()*2);
+    console.log(num);
+    $("#coin").fadeOut(500);
+    $("#coin").fadeIn(500);
+    $("#shadow").animate({width:'-20vw'}, 500);
+    $("#shadow").animate({width:'7vw'}, 500);
+    if(num===0){
+        
+        $("#coin").attr("src","./resources/heads.svg");
+        $("#result").html("Heads");
+
+    }
+    else{
+
+        $("#coin").attr("src","./resources/tails.svg");
+        $("#result").html("Tails");
+
+    }
+});
